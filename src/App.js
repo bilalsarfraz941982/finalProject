bd3fc15c39ab893a725fa3359f202db71ca8021b
Bilal Sarfraz <bilalsarfraz1982@gmail.com>
2024-01-24 11:54:16 +0000

    s

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import {Route , Routes , Link} from 'react-router-dom';
 import Homepage from './HomePage';
 import About from './About';
+import Menu from './Menu';
+import Reservation from './Reservation';
+import Orderonline from './Orderonline';
+import Login from './Login'
 
 function App() {
   return (
@@ -10,10 +14,18 @@ function App() {
       <div className='navbar'>
         <Link to="/" className='homepage'>HomePage</Link>
         <Link to="/about" className='about'>About</Link>
+        <Link to="/menu" className='menu'>Menu</Link>
+        <Link to="/reservation" className='reservation'>Reservation</Link>
+        <Link to="/orderonline" className='orderonline'>Order Online</Link>
+        <Link to="/login" className='login'>Login</Link>
       </div>
       <Routes>
         <Route path='/' element={<Homepage/>}></Route>
         <Route path='/about' element={<About/>}></Route>
+        <Route path='/menu' element={<Menu/>}></Route>
+        <Route path='/reservation' element={<Reservation/>}></Route>
+        <Route path='/orderonline' element={<Orderonline/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
       </Routes>
     </>
   )
