0d5a01ddecd95c0ead2761c545261cb198950744
Bilal Sarfraz <bilalsarfraz1982@gmail.com>
2024-01-24 11:29:47 +0000

    s

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import Welcomepage from './Welcomepage';
-import Aboutme from './Aboutme';
 import {Route , Routes , Link} from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <div className='navbar'>
-         <Link to="/" className='homepage'>Home page</Link>
-         <Link to="/aboutme" className='aboutme'>About me</Link>
-      </div>
-      <Routes>
-        <Route path='./' element={<Welcomepage/>}></Route>
-        <Route path='/aboutme'element={<Aboutme/>}></Route>
-      </Routes>
+      
     </>
   )
 }
