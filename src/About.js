95278bf05b97d014390e448f6e818893e41ff051
Bilal Sarfraz <bilalsarfraz1982@gmail.com>
2024-01-24 11:45:25 +0000

    s

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+        <h1>i am bilal lives in manchester</h1>
+    </div>
+  )
+}
+
+export default About
--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './App.css';
 import {Route , Routes , Link} from 'react-router-dom';
+import Homepage from './HomePage';
+import About from './About';
 
 function App() {
   return (
     <>
-      
+      <div className='navbar'>
+        <Link to="/" className='homepage'>HomePage</Link>
+        <Link to="/about" className='about'>About</Link>
+      </div>
+      <Routes>
+        <Route path='/' element={<Homepage/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+      </Routes>
     </>
   )
 }
