2258fe54faad01bfa4b930ba560c284087206740
Bilal Sarfraz <bilalsarfraz1982@gmail.com>
2024-01-24 14:56:29 +0000

    s

--- src/HomePage.js
+++ src/HomePage.js
@@ -4,6 +4,10 @@ import Reservation from './Reservation';
 import Menu from './Menu'
 import picture from './Assets/headerbg.jpg';
 import resturant from './Assets/restauranfood.jpg';
+import salad from './Assets/greek salad.jpg'
+import basket from './Assets/Basket.svg'
+import brunch from './Assets/bruchetta.jpg'
+import dessert from  './Assets/lemon dessert.jpg'
 import {Route , Routes,Link} from 'react-router-dom'
 
 function HomePage() {
@@ -33,6 +37,46 @@ function HomePage() {
       <Routes>
         <Route path='/menu' element={<Menu/>}></Route>
       </Routes>
+    </div>
+    <div className='mainM'>
+        <div className='firstbox'>
+            <img src={salad} alt='salad picture' height={"200px"} width={"240px"} style={{borderRadius:"20px",borderBottomRightRadius:"0px",borderBottomLeftRadius:"0px"}}/><br></br>
+            <div className='nameprice'>
+               <h4>Greek Salad</h4>
+               <h4 style={{color:"rgb(238,153,114)"}}>$12.99</h4>
+            </div>
+            <p>The famous greek salad of <br></br>crispy lettuce, peppers, olives<br></br> and our Chicago style feta <br></br>cheese, garnished with <br></br>crunchy garlic and rosemary <br></br>croutons. </p><br></br>
+            <div className='basket'>
+              <h4>Order a delivery</h4>
+              <img src={basket} alt='basket picture' height={"20px"} width={"20px"}/>
+            </div>
+        </div>
+        <div className='secondbox'>
+            <img src={brunch} alt='salad picture' height={"200px"} width={"240px"} style={{borderRadius:"20px",borderBottomRightRadius:"0px",borderBottomLeftRadius:"0px"}}/>
+            <div className='nameprice'>
+              <h4>Brunchetta</h4>
+              <h4 style={{color:"rgb(238,153,114)",marginLeft:"80px"}}>$5.99</h4>
+            </div>
+            <p>Our Bruschetta is made from <br></br> grilled bread that has been<br></br> smeared with garlic and<br></br> seasoned with salt and olive<br></br> oil.</p><br></br>
+            <br></br>
+            <div className='basket'>
+              <h4>Order a delivery</h4>
+              <img src={basket} alt='basket picture' height={"20px"} width={"20px"}/>
+            </div>
+        </div>
+        <div className='thiredbox'>
+            <img src={dessert} alt='salad picture' height={"200px"} width={"240px"} style={{borderRadius:"20px",borderBottomRightRadius:"0px",borderBottomLeftRadius:"0px"}}/>
+            <div className='nameprice'>
+            <h4>Lemon dessert</h4>
+            <h4 style={{color:"rgb(238,153,114)"}}>$5.99</h4>
+            </div>
+            <p>This comes straight from <br></br>grandma’s recipe book, every<br></br> last ingredient has been<br></br> sourced and is as authentic <br></br>as can be imagined. </p><br></br>
+            <br></br>
+            <div className='basket'>
+              <h4>Order a delivery</h4>
+              <img  src={basket} alt='basket picture' height={"20px"} width={"20px"} />
+            </div>
+        </div>
     </div>
     </>
   )
