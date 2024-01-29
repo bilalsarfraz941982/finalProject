64c5bbe0fe1672e321a61aa3548618d3c1522108
Bilal Sarfraz <bilalsarfraz1982@gmail.com>
2024-01-29 12:20:51 +0000

    s

--- src/Reservation.js
+++ src/Reservation.js
@@ -3,20 +3,21 @@ import './Reservation.css'
 
 function Reservation() {
   return (
+  <>
+    <h1>BOOKING</h1>
     <div className='mainBox'>
-      <br></br>
-      <h1>Find the table for any occasion</h1>
-      <div className='fields'>
-         <div className='date'>
-          <h4>Date</h4>
-          <input type='date' style={{tabSize:"200px"}}></input>
-         </div>
-         <div className='time'>
-          <h4>Time</h4>
-          <input type='time'></input>
-         </div>
+      <div className='bookinginfo'>
+        <div className='date'>
+          <h3>Date<sup>*</sup></h3>
+          <input type='date' style={{tabSize:""}}></input>
+        </div>
+        <div className='time'></div>
+        <h3>Time<sup>*</sup></h3>
+        <input type='time'></input>
       </div>
+      <div className='image'></div>
     </div>
+  </>
   )
 }
 
