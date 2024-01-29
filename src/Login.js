ed05d393e5e75151ccc0db025e2b75439988caaf
Bilal Sarfraz <bilalsarfraz1982@gmail.com>
2024-01-29 16:54:21 +0000

    a

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import './Login.css';
+import facebook from './Assets/fb.png';
+import instagram from './Assets/insta.png';
+import snapchatt from './Assets/snap.png';
+import twitter from './Assets/twitter.png';
+import ygbackground from './Assets/ygbackground.jpg'
 
 function Login() {
   return (
-    <div>
-        <h1>please enter your login details below</h1>
+    <div className='mainbox'>
+      <div className='leftbox'>
+        <div className='topthree'>
+          <h1>Login to Your Account</h1>
+          <h4>Login using social networks</h4>
+          <div className='snlogo'>
+           <img src={facebook} height={30} width={30} alt=''/>
+           <img src={instagram} height={30} width={30} alt=''/>
+           <img src={snapchatt} height={30} width={30} alt=''/>
+           <img src={twitter} height={30} width={30} alt=''/>
+          </div>
+          <h5>OR</h5>
+          <div className='details'>
+            <form>
+              <input type='email' id='email' className='email' placeholder='Email'></input><br></br>
+              <input type='password' id='password' className='password' placeholder='Password'></input><br></br>
+              <button style={{color:"rgb(244,206,20)"}}>Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className='rightbox' style={{backgroundImage:`url(${ygbackground})`}}>
+
+      </div>
     </div>
   )
 }
