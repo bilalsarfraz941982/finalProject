4f4d686c4ce66d8d152db5dbac92b33150a3f346
Bilal Sarfraz <bilalsarfraz1982@gmail.com>
2024-01-29 17:42:24 +0000

    a

--- src/Login.js
+++ src/Login.js
@@ -19,18 +19,20 @@ function Login() {
            <img src={snapchatt} height={30} width={30} alt=''/>
            <img src={twitter} height={30} width={30} alt=''/>
           </div><br></br>
-          <h5>----------------- OR -----------------</h5>
+          <h6 style={{color:"rgb(73,94,87)", textAlign:"center"}}>---------------------------- OR ----------------------------</h6>
           <div className='details'>
             <form>
-              <input type='email' id='email' className='email' placeholder='Email'></input><br></br>
-              <input type='password' id='password' className='password' placeholder='Password'></input><br></br>
-              <button style={{color:"rgb(244,206,20)"}}>Submit</button>
+              <input type='email' id='email' className='email' placeholder='  Email' style={{backgroundColor:"rgb(234, 217, 135)",height:"30px",width:"300px" , marginBottom:"10px",marginTop:"10px", borderRadius:"20px" , marginLeft:"80px"}}></input><br></br>
+              <input type='password' id='password' className='password' placeholder='  Password' style={{backgroundColor:"rgb(234, 217, 135)",height:"30px",width:"300px", marginBottom:"10px", borderRadius:"20px", marginLeft:"80px"}}></input><br></br>
+              <button style={{color:"rgb(73,94,87)", marginLeft:"150px",marginTop:"10px", fontSize:"80%" ,borderRadius:"50px" ,height:"35px" , width:"150px"}}>Sign in</button>
             </form>
           </div>
         </div>
       </div>
-      <div className='rightbox' style={{backgroundImage:`url(${ygbackground})`, height:"400px" , width:"400px"}}>
-         <h1>New Here?</h1>
+      <div className='rightbox' style={{backgroundColor:"rgb(73,94,87)", height:"400px" , width:"400px" ,borderRadius:"500px"}}>
+         <h1 style={{color:"white", textAlign:"center" , marginTop:"85px"}}>New Here?</h1>
+         <h3 style={{color:"white" ,textAlign:'center', marginTop:"20px", fontWeight:"200"}}>Sin up and discover a great <br></br> amount of new discounts and offers!!!</h3>
+         <button style={{color:"rgb(73,94,87)", backgroundColor:"rgb(244, 206, 20)",marginLeft:"120px",marginTop:"50px", fontSize:"80%" ,borderRadius:"50px" ,height:"35px" , width:"150px"}}>Sing up</button>
       </div>
     </div>
   )
