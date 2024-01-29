3ca0c77d25d8822e4be9d5ad6843dcaffb1b8544
Bilal Sarfraz <bilalsarfraz1982@gmail.com>
2024-01-29 18:25:44 +0000

    a

--- src/Login.js
+++ src/Login.js
@@ -4,7 +4,8 @@ import facebook from './Assets/fb.png';
 import instagram from './Assets/insta.png';
 import snapchatt from './Assets/snap.png';
 import twitter from './Assets/twitter.png';
-import ygbackground from './Assets/ygbackground.jpg'
+import {Route , Routes , Link} from 'react-router-dom';
+import Singup from './Signup';
 
 function Login() {
   return (
@@ -24,15 +25,18 @@ function Login() {
             <form>
               <input type='email' id='email' className='email' placeholder='  Email' style={{backgroundColor:"rgb(234, 217, 135)",height:"30px",width:"300px" , marginBottom:"10px",marginTop:"10px", borderRadius:"20px" , marginLeft:"80px"}}></input><br></br>
               <input type='password' id='password' className='password' placeholder='  Password' style={{backgroundColor:"rgb(234, 217, 135)",height:"30px",width:"300px", marginBottom:"10px", borderRadius:"20px", marginLeft:"80px"}}></input><br></br>
-              <button style={{color:"rgb(73,94,87)", marginLeft:"150px",marginTop:"10px", fontSize:"80%" ,borderRadius:"50px" ,height:"35px" , width:"150px"}}>Sign in</button>
+              <button style={{color:"rgb(73,94,87)", backgroundColor:"rgb(244, 206, 20)", marginLeft:"150px",marginTop:"10px", fontSize:"80%" ,borderRadius:"50px" ,height:"35px" , width:"150px" , }}>Sign in</button>
             </form>
           </div>
         </div>
       </div>
       <div className='rightbox' style={{backgroundColor:"rgb(73,94,87)", height:"400px" , width:"400px" ,borderRadius:"500px"}}>
          <h1 style={{color:"white", textAlign:"center" , marginTop:"85px"}}>New Here?</h1>
-         <h3 style={{color:"white" ,textAlign:'center', marginTop:"20px", fontWeight:"200"}}>Sin up and discover a great <br></br> amount of new discounts and offers!!!</h3>
-         <button style={{color:"rgb(73,94,87)", backgroundColor:"rgb(244, 206, 20)",marginLeft:"120px",marginTop:"50px", fontSize:"80%" ,borderRadius:"50px" ,height:"35px" , width:"150px"}}>Sing up</button>
+         <h3 style={{color:"white" ,textAlign:'center', marginTop:"20px", fontWeight:"200"}}>Sin up and discover a great <br></br> amount of discounts and offers!!!</h3>
+         <Link to='/signup' className='signup'><button style={{color:"rgb(73,94,87)", backgroundColor:"rgb(244, 206, 20)",marginLeft:"120px",marginTop:"50px", fontSize:"80%" ,borderRadius:"50px" ,height:"35px" , width:"150px"}}>Sing up</button></Link>
+         <Routes>
+          <Route path='/signup' element={<Singup/>}></Route>
+         </Routes>
       </div>
     </div>
   )
