fcc190d8038c65a9fd9da428049a9b8ce56baae3
Bilal Sarfraz <bilalsarfraz1982@gmail.com>
2024-01-29 12:34:18 +0000

    a

--- src/Reservation.js
+++ src/Reservation.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import './Reservation.css'
+import './Reservation.css';
+import reservation from './Assets/reservation.jpg'
 
 function Reservation() {
   return (
-  <>
-    <h1>BOOKING</h1>
     <div className='mainBox'>
+      <h1>BOOKING</h1>
       <div className='bookinginfo'>
         <div className='date'>
-          <h3>Date<sup>*</sup></h3>
+          <h3>Date<sup style={{color:"red"}}>*</sup></h3>
           <input type='date' style={{tabSize:""}}></input>
         </div>
         <div className='time'></div>
-        <h3>Time<sup>*</sup></h3>
+        <h3>Time<sup style={{color:"red"}}>*</sup></h3>
         <input type='time'></input>
       </div>
-      <div className='image'></div>
+      <div className='image'>
+        <img alt='' src={reservation} height={"200px"} width={"200px"} />
+      </div>
     </div>
-  </>
   )
 }
 
