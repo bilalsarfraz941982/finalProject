435795a685bd2e49e56145c3e51c876ce39101dd
Bilal Sarfraz <bilalsarfraz1982@gmail.com>
2024-01-31 12:28:11 +0000

    s

--- src/Login.js
+++ src/Login.js
@@ -33,7 +33,7 @@ function Login() {
       <div className='rightbox' style={{backgroundColor:"rgb(73,94,87)", height:"400px" , width:"400px" ,borderRadius:"500px"}}>
          <h1 style={{color:"white", textAlign:"center" , marginTop:"85px"}}>New Here?</h1>
          <h3 style={{color:"white" ,textAlign:'center', marginTop:"20px", fontWeight:"200"}}>Sin up and discover a great <br></br> amount of discounts and offers!!!</h3>
-         <Link to='/signup' className='signup'><button style={{color:"rgb(73,94,87)", backgroundColor:"rgb(244, 206, 20)",marginLeft:"120px",marginTop:"50px", fontSize:"80%" ,borderRadius:"50px" ,height:"35px" , width:"150px"}}>Sing up</button></Link>
+         <Link to='/signup' className='signup'><button style={{color:"rgb(73,94,87)", backgroundColor:"rgb(244, 206, 20)",marginLeft:"120px",marginTop:"50px", fontSize:"80%" ,borderRadius:"50px" ,height:"35px" , width:"150px"}}>Sign up</button></Link>
          <Routes>
           <Route path='/signup' element={<SignUp/>}></Route>
          </Routes>
